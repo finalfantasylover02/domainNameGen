76e6d2561466745823dc49f9b97dbcb81ff67f0c
muldersgf69 <109549032+muldersgf69@users.noreply.github.com>
2024-02-23 23:30:48 +0000

    second commit

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-let pronouns = ['the', 'our'];
+window.onload = function() {
+    let pronouns = ['the', 'our'];
+    let adjs = ['great', 'big'];
+    let nouns = ['jogger', 'racoon'];
 
-  let adjs = ['great', 'big'];
+    let domainList = document.getElementById('domainList');
 
-  let nouns = ['jogger', 'racoon'];
-
-
-for (let i = 0; i < pronouns.length; i++) {
-    for (let j = 0; j < adjs.length; j++) {
-        for (let k = 0; k < nouns.length; k++) {
-            let domainName = pronouns[i] + adjs[j] + nouns[k] + '.com';
-            console.log(domainName);
+    for (let i = 0; i < pronouns.length; i++) {
+        for (let j = 0; j < adjs.length; j++) {
+            for (let k = 0; k < nouns.length; k++) {
+                let domainName = pronouns[i] + adjs[j] + nouns[k] + '.com';
+                let listItem = document.createElement('li');
+                listItem.textContent = domainName;
+                domainList.appendChild(listItem);
+            }
         }
     }
-}
+};
